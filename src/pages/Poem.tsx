000585e9f8836e2cfd04d69c673d717f7d27159f
Nauman Umer <nmanumr@gmail.com>
2024-10-06 22:57:10 +0500

    Refine poem page layout
    Adjust padding, margins, and spacing to enhance readability. Updated classes aim to create a cleaner and more consistent visual presentation of the poem content.

--- src/pages/Poem.tsx
+++ src/pages/Poem.tsx
@@ -7,7 +7,7 @@ export default function Poem() {
 
   return (
     <div
-      className="text-center font-nastaliq leading-[2] py-10 px-4 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12"
+      className="text-center font-nastaliq leading-[2] py-10 px-4 sm:px-6 md:px-4 lg:px-12"
       dir="rtl"
     >
       <div dir="rtl" className="text-center leading-[2] font-nastaliq mb-10">
@@ -49,7 +49,7 @@ function Verses({poem}: { poem: Element }) {
                   ?.map((v) => v.trim())
                   ?.filter(Boolean)
                   ?.map((ver) => (
-                    <div className="even:mb-4 flex justify-center">
+                    <div className="even:mb-2 flex justify-center">
                       <Verse content={ver}/>
                     </div>
                   ))}
@@ -86,7 +86,7 @@ function Verse({content}: { content: string }) {
           .split(" ")
           .filter(Boolean)
           .map((word) => (
-            <div className="inline-block">
+            <div className="inline-block px-0.5">
               {word}&nbsp;
             </div>
           ))
