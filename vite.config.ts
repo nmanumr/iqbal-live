c6d195988b9d3ededf3bd81cd106bb5fc1804d32
Nauman Umer <nmanumr@gmail.com>
2024-10-06 15:15:14 +0500

    Add conditional base path for non-serve commands in Vite config
    This update modifies the Vite configuration to set a base path only when the command is not 'serve'. This ensures that the base path is correctly configured for production builds without affecting the development server.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig, type UserConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config: UserConfig = {
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    config.base = '/<your-repo-name>/'
+  }
+
+  return config
 })
